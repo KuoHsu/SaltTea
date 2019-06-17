ff5ff86e6e4d24691b59de1368dcd75ac9013a45
KuoHsu <33677654+KuoHsu@users.noreply.github.com>
2019-06-17 11:50:40 +0800

    'QueryReportFinish'

--- front/static/js/extend-functions.js
+++ front/static/js/extend-functions.js
@@ -26,9 +26,18 @@ function priceFormat(price) {
   return formatter.format("($  0,0)");
 }
 
+function numberFormat(number) {
+  var formatter = numeral(number);
+  return formatter.format("0,0");
+}
+
+function fontBolder(tableId, rowIndex) {
+  $("#" + tableId + " tr")[rowIndex].style.fontWeight = "bolder";
+}
+
 function tdCreater(
   text,
-  fontSize = "12px",
+  fontSize = "20px",
   textAlign = "center",
   borderBottom = "None",
   colSpan = 1
@@ -42,8 +51,13 @@ function tdCreater(
   return td;
 }
 
+function branchName(branchId) {
+  return Number(branchId) - 20000 + "號分店";
+}
+
 function createOperationTable(table, dataObj, isBranch = false) {
-  $("#" + table).html("");
+  var __table = "#" + table;
+  $(__table).html("");
 
   var trString = stringCopy("<tr/>", 12);
   var trs = $(trString);
@@ -63,59 +77,209 @@ function createOperationTable(table, dataObj, isBranch = false) {
     "2019年" +
     dataObj.month +
     "月" +
-    (isBranch ? "臺科大分店營運概況表" : "總體營運概況表");
+    (isBranch ? branchName(dataObj.branchId) + "銷售詳情表" : "總體營運概況表");
 
   var info =
     (isBranch ? "分店代碼：" + dataObj.branchId : "") +
-    "報表期間：2019年" +
+    "　報表期間：2019年" +
     dataObj.month +
     "月";
 
-  $(trs[0]).append(tdCreater("鹽茶", "20px", "center", "none", 2));
+  $(trs[0]).append(tdCreater("鹽茶", "32px", "center", "none", 2));
   $(trs[0]).css("font-weight", "bolder");
   $(trs[1]).append(
-    tdCreater(subtitle, "16px", "center", borderStyle["bolder"], 2)
+    tdCreater(subtitle, "28px", "center", borderStyle.bolder, 2)
   );
   $(trs[1]).css("font-weight", "bolder");
 
-  $(trs[2]).append(tdCreater(info, "8px", "right", "none", 2));
+  $(trs[2]).append(tdCreater(info, "14px", "right", "none", 2));
 
-  $(trs[3]).append(tdCreater("單位：新台幣元", "8px", "right", "none", 2));
+  $(trs[3]).append(tdCreater("單位：新台幣元", "14px", "right", "none", 2));
 
-  $(trs[4]).append(tdCreater("　", "8px", "center", "none", 2));
+  $(trs[4]).append(tdCreater("　", "16px", "center", "none", 2));
 
-  $(trs[5]).append(tdCreater("項目", "14px", "center", borderStyle["normal"]));
-  $(trs[5]).append(tdCreater("總額", "14px", "center", borderStyle["normal"]));
+  $(trs[5]).append(tdCreater("項目", "24px", "center", borderStyle.normal));
+  $(trs[5]).append(tdCreater("總額", "24px", "center", borderStyle.normal));
   $(trs[5]).css("font-weight", "bolder");
 
-  $(trs[6]).append(tdCreater("銷貨收入", "12px", "center", "none"));
-  $(trs[6]).append(tdCreater(priceFormat(rc[0]), "12px", "right", "none"));
+  $(trs[6]).append(tdCreater("銷貨收入", "20px", "center", "none"));
+  $(trs[6]).append(tdCreater(priceFormat(rc[0]), "20px", "right", "none"));
 
-  $(trs[7]).append(tdCreater("銷貨成本", "12px", "center", "none"));
-  $(trs[7]).append(tdCreater(priceFormat(rc[1]), "12px", "right", "none"));
+  $(trs[7]).append(tdCreater("銷貨成本", "20px", "center", "none"));
+  $(trs[7]).append(tdCreater(priceFormat(rc[1]), "20px", "right", "none"));
 
-  $(trs[8]).append(tdCreater("薪資支出", "12px", "center", "none"));
-  $(trs[8]).append(tdCreater(priceFormat(rc[2]), "12px", "right", "none"));
+  $(trs[8]).append(tdCreater("薪資支出", "20px", "center", "none"));
+  $(trs[8]).append(tdCreater(priceFormat(rc[2]), "20px", "right", "none"));
 
-  $(trs[9]).append(tdCreater("水電瓦斯費", "12px", "center", "none"));
-  $(trs[9]).append(tdCreater(priceFormat(rc[3]), "12px", "right", "none"));
+  $(trs[9]).append(tdCreater("水電瓦斯費", "20px", "center", "none"));
+  $(trs[9]).append(tdCreater(priceFormat(rc[3]), "20px", "right", "none"));
 
   $(trs[10]).append(
-    tdCreater("租金費用", "12px", "center", borderStyle["double"])
+    tdCreater("租金費用", "20px", "center", borderStyle.double)
   );
   $(trs[10]).append(
-    tdCreater(priceFormat(rc[4]), "12px", "right", borderStyle["double"])
+    tdCreater(priceFormat(rc[4]), "20px", "right", borderStyle.double)
   );
 
-  $(trs[11]).append(tdCreater("總計", "14px", "center", "none"));
-  $(trs[11]).append(tdCreater(priceFormat(total), "14px", "right", "none"));
+  $(trs[11]).append(tdCreater("總計", "24px", "center", "none"));
+  $(trs[11]).append(tdCreater(priceFormat(total), "24px", "right", "none"));
 
   console.log(trs);
 
-  $("#" + table).append(trs);
-  $("#" + table).css("border", "dotted 2px #111");
-  $("#" + table + " td").css("width", "5%");
-  $("#" + table + " td").css("padding", "0 6px");
+  $(__table).append(trs);
+  $(__table).css("border", "dotted 2px #111");
+  $(__table + " td").css("width", "300px");
+  $(__table + " td").css("padding", "0 6px");
 }
 
-function createSalesTable(isBranch = false) {}
+function createSalesTable(table, dataObj, isBranch = false) {
+  function getSalesTableItemTitle() {
+    tr = $("<tr/>");
+    title = ["商品編號", "商品名稱", "商品群組", "單價", "銷售量", "銷售額"];
+    for (var i = 0; i < 6; i++) {
+      $(tr).append(tdCreater(title[i], "20px", "center"));
+    }
+    $(tr).css("border-bottom", borderStyle.normal);
+    return tr;
+  }
+
+  function getSalesTableGroupInfo(groupId, items) {
+    var data = { quantity: 0, sales: 0, rows: [] };
+    var totalQuantity = 0;
+    var totalSales = 0;
+    var rows = data.rows;
+    var fontSize = "20px";
+    var amountOfItem = items.length;
+    var count = 1;
+
+    /*itemInfo*/
+    for (var key in items) {
+      item = items[key];
+      tr = $("<tr/>");
+      /*Id, name, group*/
+      $(tr).append(tdCreater(item.itemId, fontSize, "cneter", "none"));
+      $(tr).append(tdCreater(item.name, fontSize, "cneter", "none"));
+      $(tr).append(tdCreater(groupId, fontSize, "cneter", "none"));
+
+      /*price, quantity, sales*/
+      var sales = item.price * item.quantity;
+      tdPrice = tdCreater(priceFormat(item.price), fontSize, "right", "none");
+      tdQuantity = tdCreater(
+        numberFormat(item.quantity),
+        fontSize,
+        "right",
+        "none"
+      );
+      tdSales = tdCreater(priceFormat(sales), fontSize, "right", "none");
+      $(tr).append(tdPrice);
+      $(tr).append(tdQuantity);
+      $(tr).append(tdSales);
+
+      /*last row should draw the double-line under the data.*/
+      if (count == amountOfItem) {
+        $(tdQuantity).css("border-bottom", borderStyle.double);
+        $(tdSales).css("border-bottom", borderStyle.double);
+      }
+
+      totalQuantity += item.quantity;
+      totalSales += sales;
+      rows.push(tr);
+      count++;
+    }
+    /*itemInfo end*/
+
+    /*amount*/
+    totalTr = $("<tr/>");
+    $(totalTr).append(tdCreater("小計", fontSize, "right", "none", 4));
+    $(totalTr).append(
+      tdCreater(numberFormat(totalQuantity), fontSize, "right", "none")
+    );
+    $(totalTr).append(
+      tdCreater(priceFormat(totalSales), fontSize, "right", "none")
+    );
+    rows.push(totalTr);
+    emptyTr = $("<tr/>");
+    $(emptyTr).append(tdCreater("　", fontSize, "right", "none", 6));
+    rows.push(emptyTr);
+    /*amount end*/
+    data.quantity = totalQuantity;
+    data.sales = totalSales;
+
+    return data;
+  }
+
+  __table = "#" + table;
+  $(__table).html("");
+  var trString = stringCopy("<tr/>", 5);
+  var trs_title = $(trString);
+
+  var subtitle =
+    "2019年" +
+    dataObj.month +
+    "月" +
+    (isBranch ? branchName(dataObj.branchId) + "銷售詳情表" : "總體銷售詳情表");
+
+  var info =
+    (isBranch ? "分店代碼：" + dataObj.branchId : "") +
+    "　報表期間：2019年" +
+    dataObj.month +
+    "月";
+
+  /*Table Header*/
+  $(trs_title[0]).append(tdCreater("鹽茶", "32px", "center", "none", 6));
+  $(trs_title[0]).css("font-weight", "bolder");
+  $(trs_title[1]).append(
+    tdCreater(subtitle, "28px", "center", borderStyle.bolder, 6)
+  );
+  $(trs_title[1]).css("font-weight", "bolder");
+
+  $(trs_title[2]).append(tdCreater(info, "14px", "right", "none", 6));
+
+  $(trs_title[3]).append(
+    tdCreater("單位：新台幣元", "14px", "right", "none", 6)
+  );
+
+  $(trs_title[4]).append(tdCreater("　", "16px", "center", "none", 6));
+  $(__table).append(trs_title);
+  $(__table).append(getSalesTableItemTitle);
+  /*Table Header end*/
+
+  /*itemInfos*/
+
+  var groups = dataObj.groups;
+  var totalQuantity = 0;
+  var totalSales = 0;
+  for (var groupId in groups) {
+    data = getSalesTableGroupInfo(groupId, groups[groupId]);
+    rows = data.rows;
+    for (var index in rows) $(__table).append(rows[index]);
+    totalQuantity += data.quantity;
+    totalSales += data.sales;
+  }
+  /*itemInfos end*/
+
+  /*total*/
+  totalTr = $("<tr/>");
+
+  $(totalTr).append(tdCreater("總計", "24px", "right", "none", 4));
+  $(totalTr).append(
+    tdCreater(numberFormat(totalQuantity), "24px", "right", "none")
+  );
+  $(totalTr).append(
+    tdCreater(priceFormat(totalSales), "24px", "right", "none")
+  );
+
+  $(__table).append(totalTr);
+
+  $("#queryReport tr")
+    .last()
+    .css("border-top", borderStyle.double);
+  /*total end */
+
+  $(__table + " td").css("width", "5%");
+  rowCount = $(__table + " tr").length;
+  fontBolder(table, 0);
+  fontBolder(table, 1);
+  fontBolder(table, 5);
+  fontBolder(table, rowCount - 1);
+}
